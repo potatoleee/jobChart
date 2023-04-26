1b05b3278ede6b032fe07f5221bd4624af608b81
potatolee <dean19990226@gmail.com>
2023-04-26 22:57:42 +0800

    部署測試

--- deploy.sh
+++ deploy.sh
@@ -19,10 +19,10 @@ git init
 git checkout -B main
 git add -A
 git commit -m 'deploy'
-//部署至main
+
 # if you are deploying to https://potatoleee.github.io
 # git push -f git@github.com:potatoleee/potatoleee.github.io.git main
-//部署至gh-pages 先選這個
+
 # if you are deploying to https://potatoleee.github.io/<REPO>
 git push -f git@github.com:potatoleee/jobChart.git main:gh-pages
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,10 @@
 
 <template>
   <main>
-    <h1>2021 薪資數據圖表調查</h1>
+    <h1>2021 薪資數據圖表調查1</h1>
     <button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
+
+
     
   </main>
 </template>
